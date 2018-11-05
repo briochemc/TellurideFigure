d07f222b9e2a3f31410c88190aa8317c0a829f99
Benoit Pasquier <pasquieb@uci.edu>
2018-11-05 15:08:43 -0800

    little progress made

--- figure1bis.jl
+++ figure1bis.jl
@@ -1,6 +1,13 @@
 using StatsPlots
 using DataFrames
 
-goal is to use https://github.com/JuliaPlots/StatPlots.jl
 
+using Compat, Random, Distributions
+Random.seed!(123)
 
+using Plots
+μ = 10
+σ = 2
+dist_FeT = LogNormal(μ, σ)  # Find correct values for μ and σ
+sample_FeT = rand(dist_FeT, 10000)
+histogram(log10.(sample_FeT), bin = 100)

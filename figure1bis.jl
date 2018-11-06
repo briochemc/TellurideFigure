db99aae51ba3113e05b5eec752cbde4693a4e1fb
Benoit Pasquier <pasquieb@uci.edu>
2018-11-05 17:15:45 -0800

    Playing around, didnt get much done

--- figure1bis.jl
+++ figure1bis.jl
@@ -1,13 +1,19 @@
-using StatsPlots
-using DataFrames
+using StatPlots, DataFrames, MultivariateStats, Compat, Random, Distributions, Plots
 
+# Find observed μ and σ for FeT
+μ_FeT = log10(100)
+σ_FeT = 2
+# Find observed μ and σ for DFe
+μ_DFe = log10(1)
+σ_DFe = 2
+# Create distributions
+dist_FeT = LogNormal(μ_FeT, σ_FeT)
+dist_DFe = LogNormal(μ_DFe, σ_DFe)
+# Sample from distributions and put in DataFrame
+n = 10000
+M = DataFrame()
+M[:DFe] = rand(dist_DFe, n)
+M[:FeT] = rand(dist_FeT, n)
+M[:valid] = M[:DFe] .≤ M[:FeT]
+head(M)
 
-using Compat, Random, Distributions
-Random.seed!(123)
-
-using Plots
-μ = 10
-σ = 2
-dist_FeT = LogNormal(μ, σ)  # Find correct values for μ and σ
-sample_FeT = rand(dist_FeT, 10000)
-histogram(log10.(sample_FeT), bin = 100)
